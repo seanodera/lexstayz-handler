6fbef6335522b2aa7009d0569751639f07690c37
seanolero <seanolero@outlook.com>
2025-01-15 00:14:37 +0300

    Payment controller updated

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -268,13 +268,14 @@ exports.createWithdrawalAccount = async (req, res) => {
                     service: 'Pawapay'
                 };
             }
-
-            res.status(200).json({
+            console.log(withdrawalAccount);
+            return res.status(200).json({
                 status: 'success',
                 data: withdrawalAccount
             })
     } catch (error){
-
+        console.log(error)
+        return res.status(500).json({error: error.message});
     }
 }
 
